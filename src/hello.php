71506e67e88de9e93eca9efe490dd20d0e495062
Peter D <oranjepeter@hotmail.com>
2018-05-28 20:00:20 +0200

    Create hello.php

---
+++ src/hello.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Database;
+
+/**
+ * A terribly useful class
+ *
+ * See: http://knpuniversity.com/screencast/question-answer-day
+ */
+class Hello
+{
+    public function sayHallo()
+    {
+        return 'Hallo!';
+    }
+}
