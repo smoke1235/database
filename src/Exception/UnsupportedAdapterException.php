55a27ce3be33700a55c05697c98ceb74be89a617
Peter D <oranjepeter@hotmail.com>
2018-07-26 18:37:55 +0200

    Create UnsupportedAdapterException.php

---
+++ src/Exception/UnsupportedAdapterException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Part of the Database Package
+ *
+ * @copyright  Copyright (C) 2018
+ */
+
+namespace Smoke\Database\Exception;
+
+/**
+ * Exception class defining an unsupported database object
+ *
+ */
+class UnsupportedAdapterException extends \RuntimeException
+{
+}
